b9636a0e000619e73f1c5a1f25514a98de1063a7
Anthony2be <adubovitsky1234@gmail.com>
2021-10-26 09:23:32 -0700

    try gh pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import  SideBar from './Sidebar';
-import SideBarIcon from './Sidebar';
-import {FaFire} from 'react-icons/fa';
+import SideBar from './Sidebar';
+
+import {FaFire, FaAccusoft, FaAirbnb, FaDiscord, FaMicrosoft} from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="flex">
-    <SideBar>
-    </SideBar>
+    <div className="flex h-screen w-screen bg-gray-600">
+      <SideBar>
+        <SideBarIcon icon={<FaFire size='28'/>}/>
+        <SideBarIcon icon={<FaDiscord size='30'/>}/>
+        <SideBarIcon icon={<FaAccusoft size='28'/>}/>
+        <SideBarIcon icon={<FaAirbnb size='28'/>}/>
+        <SideBarIcon icon={<FaMicrosoft size='28'/>}/>
+      </SideBar>
+      <div className="ml-[5em] text-white mt-[0.75em] w-screen h-screen text-center">
+        <h1>hello world</h1>
+      </div>
     </div>
   );
 }
+
+const SideBarIcon = ({icon}) =>(
+  <div className='sidebar-icon'>
+      {icon}
+  </div>
+);
 
 export default App;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,18 +1,15 @@
 import './index.css'
 import { FaFire } from 'react-icons/fa'
 
-const SideBar = () => {
+const SideBar = (props) => {
+    //<SideBarIcon icon={<FaFire size='28'/>}/>
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
-            <SideBarIcon icon={<FaFire size='28'/>}/>
+            {props.children}
         </div>
     )
 };
 
-const SideBarIcon = ({icon}) =>(
-    <div className='sidebar-icon'>
-        {icon}
-    </div>
-);
 
-export default (SideBar,SideBarIcon);
+
+export default (SideBar);
